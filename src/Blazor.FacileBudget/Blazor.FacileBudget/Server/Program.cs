ff3805f80f1ef3a0216323448abcc32eedf29637
Angelo Pirola <angelo9871@gmail.com>
2022-02-20 16:19:33 +0100

    Aggiunto progetto FacileBudget

---
+++ src/Blazor.FacileBudget/Blazor.FacileBudget/Server/Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blazor.FacileBudget.Server
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateWebHostBuilder(string[] args) => 
+            Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webHostBuilder => { 
+                webHostBuilder.UseStartup<Startup>(); // Usare solo per il debug, lasciare commentato per il deploy su IIS
+
+                //// Per eseguire il deploy su IIS, decommentare il codice sottostante e lanciare il comando: dotnet publish --configuration Release
+                //webHostBuilder.UseStartup<Startup>()
+                //.UseSerilog((webHostBuilderContext, loggerConfiguration) => 
+                //{
+                //    loggerConfiguration.ReadFrom.Configuration(webHostBuilderContext.Configuration);
+                //});
+            });
+    }
+}
---
+++ src/Blazor.FacileBudget/Blazor.FacileBudget/Server/Startup.cs
@@ -0,0 +1,52 @@
+using Blazor.FacileBudget.Validation;
+using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Blazor.FacileBudget.Server
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc()
+                .AddFluentValidation(option => {
+                    option.RegisterValidatorsFromAssemblyContaining<SpeseCreateValidator>();
+                });
+
+            services.AddRazorPages();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseWebAssemblyDebugging();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseBlazorFrameworkFiles();
+
+            app.UseStaticFiles();
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapRazorPages();
+                endpoints.MapControllers();
+                endpoints.MapFallbackToFile("index.html");
+            });
+        }
+    }
+}
