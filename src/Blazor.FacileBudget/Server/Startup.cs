326d407afa6afde4b9690dccea1ceb616c80f501
Angelo Pirola <angelo9871@gmail.com>
2022-02-27 20:55:03 +0100

    Aggiunto Swagger

--- src/Blazor.FacileBudget/Server/Startup.cs
+++ src/Blazor.FacileBudget/Server/Startup.cs
@@ -1,3 +1,4 @@
+using Blazor.FacileBudget.DataAccess.Models.Services.Application;
 using Blazor.FacileBudget.DataAccess.Models.Services.Infrastructure;
 using Blazor.FacileBudget.Validation;
 using FluentValidation.AspNetCore;
@@ -7,6 +8,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
 namespace Blazor.FacileBudget.Server
 {
@@ -29,11 +31,47 @@ namespace Blazor.FacileBudget.Server
 
             services.AddRazorPages();
 
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", builder =>
+                {
+                    builder.AllowAnyOrigin()
+                    .AllowAnyHeader()
+                    .AllowAnyMethod();
+                });
+            });
+
             services.AddDbContextPool<FacileBudgetDbContext>(optionsBuilder =>
             {
                 string connectionString = Configuration.GetSection("ConnectionStrings").GetValue<string>("Default");
                 optionsBuilder.UseSqlite(connectionString);
             });
+
+            services.AddSwaggerGen(config =>
+            {
+                config.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Blazor.FacileBudget",
+                    Version = "v1",
+                    Description = "API that allows budget management",
+                    // TermsOfService = new Uri("https://example.com/terms"), 
+
+                    //Contact = new OpenApiContact
+                    //{
+                    //    Name = "Nominativo contatto",
+                    //    Email = "Email contatto",
+                    //    Url = new Uri("https://twitter.com/username-contatto"),
+                    //},
+
+                    // License = new OpenApiLicense
+                    // {
+                    //     Name = "Nome licenza API",
+                    //     Url = new Uri("https://example.com/license"),
+                    // }
+                });
+            });
+
+            services.AddTransient<ISpesaService, EfCoreSpesaService>();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -42,6 +80,9 @@ namespace Blazor.FacileBudget.Server
             {
                 app.UseDeveloperExceptionPage();
                 app.UseWebAssemblyDebugging();
+
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Blazor.FacileBudget v1"));
             }
 
             app.UseHttpsRedirection();
@@ -50,6 +91,7 @@ namespace Blazor.FacileBudget.Server
             app.UseStaticFiles();
             app.UseRouting();
 
+            app.UseCors("CorsPolicy");
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapRazorPages();
