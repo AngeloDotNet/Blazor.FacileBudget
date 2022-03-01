2486003790a563cb2aec2da2ca1f74ad7a25f3c5
Angelo Pirola <angelo9871@gmail.com>
2022-03-01 11:24:45 +0100

    Aggiunto servizio applicativo parte client

---
+++ src/Blazor.FacileBudget/Client/Models/Services/Application/ISpesaService.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace Blazor.FacileBudget.Client.Models.Services.Application
+{
+    public interface ISpesaService
+    {
+        Task GetAllSpese();
+    }
+}
--- src/Blazor.FacileBudget/Client/Program.cs
+++ src/Blazor.FacileBudget/Client/Program.cs
@@ -1,3 +1,4 @@
+using Blazor.FacileBudget.Client.Models.Services.Application;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using System;
@@ -14,6 +15,7 @@ namespace Blazor.FacileBudget.Client
             builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddScoped<ISpesaService, SpesaService>();
 
             await builder.Build().RunAsync();
         }
