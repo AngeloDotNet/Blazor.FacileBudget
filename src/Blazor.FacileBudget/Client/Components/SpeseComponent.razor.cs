0671255fc8beca118e5825c4ae1e02d5a1b90d88
Angelo Pirola <angelo9871@gmail.com>
2022-03-15 19:52:35 +0100

    Refactoring Components

---
+++ src/Blazor.FacileBudget/Client/Components/SpeseComponent.razor.cs
@@ -0,0 +1,33 @@
+﻿using Blazor.FacileBudget.Models.ViewModels;
+using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
+
+namespace Blazor.FacileBudget.Client.Components
+{
+    public partial class SpeseComponent : ComponentBase
+    {
+        [Parameter]
+        public List<SpesaViewModel> Spese { get; set; }
+
+        [Parameter]
+        public int RowsPerPage { get; set; }
+
+        [Parameter]
+        public int[] PageSizeOptions { get; set; }
+
+        [Parameter]
+        public bool HideRowsPerPage { get; set; }
+
+        [Parameter]
+        public bool HidePageNumber { get; set; }
+
+        [Parameter]
+        public bool HidePagination { get; set; }
+
+        [Parameter]
+        public bool HasError { get; set; }
+
+        [Parameter]
+        public bool IsLoading { get; set; }
+    }
+}
